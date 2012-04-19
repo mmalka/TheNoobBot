283c394a68c997e555f1e706b4316f62644a8124
riname <riname@riname.fr>
2012-04-19 13:47:16 +0200

    mise à jour 4.3.4

--- The Noob Bot/nManager/Information.cs
+++ The Noob Bot/nManager/Information.cs
@@ -2,8 +2,8 @@
 {
     public static class Information
     {
-        public const string ForWow = "4.3.3";
-        public const int ForBuildWowVersion = 15354;
-        public const string Version = "1.0 Beta 14";
+        public const string ForWow = "4.3.4";
+        public const int ForBuildWowVersion = 15595;
+        public const string Version = "1.0 Beta 15";
     }
 }
