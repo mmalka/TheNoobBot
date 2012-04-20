99c14cbded30bdcd9e307af6311686e78e863113
Sundark <contact@thenoobcompany.com>
2012-04-20 04:20:14 +0200

    *Rename de Traker en Tracker. *Ajout d'un dossier "release" qui contient fichiers de langues, profiles livrés avec le bot, base de données de nodes, il faudra ajouter les DLL produits + nManager.dll et TheNoobBot.exe pour faire une mise à jour du bot.

---
+++ The Noob Bot/Tracker/Main.cs
@@ -0,0 +1,94 @@
+﻿// ReSharper disable CheckNamespace
+
+using System;
+using System.Windows.Forms;
+using nManager.Helpful;
+using nManager.Products;
+using Tracker;
+
+public class Main : IProduct
+// ReSharper restore CheckNamespace
+{
+    #region IProduct Members
+
+    private FormTracker formTracker = null;
+
+    public void Initialize()
+    {
+        try
+        {
+            Logging.Status = "Initialize Tracker Complete";
+            Logging.Write("Initialize Tracker Complete");
+        }
+        catch (Exception e)
+        {
+            Logging.WriteError("Tracker > Main > Initialize(): " + e);
+        }
+    }
+
+    public void Dispose()
+    {
+        try
+        {
+            Stop();
+            Logging.Status = "Dispose Tracker Complete";
+            Logging.Write("Dispose Tracker Complete");
+        }
+        catch (Exception e)
+        {
+            Logging.WriteError("Tracker > Main > Dispose(): " + e);
+        }
+    }
+
+    public void Start()
+    {
+        try
+        {
+            formTracker = new FormTracker();
+            formTracker.Show();
+            _isStarted = true;
+            Logging.Status = "Tracker started";
+        }
+        catch (Exception e)
+        {
+            Logging.WriteError("Tracker > Main > Start(): " + e);
+        }
+    }
+
+    public void Stop()
+    {
+        try
+        {
+            formTracker.Dispose();
+            _isStarted = false;
+            Logging.Status = "Tracker stoped";
+        }
+        catch (Exception e)
+        {
+            Logging.WriteError("Tracker > Main > Stop(): " + e);
+        }
+    }
+
+    public void Settings()
+    {
+        try
+        {
+            MessageBox.Show(nManager.Translate.Get(nManager.Translate.Id.No_setting_for_this_product));
+            Logging.Status = "Settings Tracker Complete";
+            Logging.Write("Settings Tracker Complete");
+        }
+        catch (Exception e)
+        {
+            Logging.WriteError("Tracker > Main > Settings(): " + e);
+        }
+    }
+
+    public bool IsStarted
+    {
+        get { return _isStarted; }
+    }
+
+    private bool _isStarted;
+
+    #endregion
+}
