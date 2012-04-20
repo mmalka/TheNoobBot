57bf0afbf65a0b2a65a4bfb385dd96db4608d241
Sundark <contact@thenoobcompany.com>
2012-04-20 04:12:26 +0200

    Cleanup du dossier source.

---
+++ The Noob Bot/Disenchanting/Disenchanting.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Windows.Forms;
+using nManager.Helpful;
+
+namespace Disenchanting
+{
+    class Disenchanting
+    {
+        public static void Pulse()
+        {
+            var thread = new Thread(ThreadPulse) { Name = "Thread Disenchanting" };
+            thread.Start();
+        }
+
+        static void ThreadPulse()
+        {
+            if (nManager.nManagerSetting.CurrentSetting.DisenchantingList.Count <= 0)
+            {
+                MessageBox.Show(nManager.Translate.Get(nManager.Translate.Id.Please_add_items_at_prospected_in__General_Settings_____Looting_____Disenchanting_List));
+                nManager.Products.Products.ProductStop();
+                return;
+            }
+
+            var Disenchanting = new nManager.Wow.Bot.States.DisenchantingState();
+            Disenchanting.Run();
+            Logging.Write("Disenchanting finished.");
+            nManager.Products.Products.ProductStop();
+        }
+    }
+}
---
+++ The Noob Bot/Disenchanting/Main.cs
@@ -0,0 +1,89 @@
+﻿// ReSharper disable CheckNamespace
+
+using System;
+using System.Windows.Forms;
+using nManager.Helpful;
+using nManager.Products;
+
+public class Main : IProduct
+// ReSharper restore CheckNamespace
+{
+    #region IProduct Members
+
+    public void Initialize()
+    {
+        try
+        {
+            Logging.Status = "Initialize Disenchanting Complete";
+            Logging.Write("Initialize Disenchanting Complete");
+        }
+        catch (Exception e)
+        {
+            Logging.WriteError("Disenchanting > Main > Initialize(): " + e);
+        }
+    }
+
+    public void Dispose()
+    {
+        try
+        {
+            Stop();
+            Logging.Status = "Dispose Disenchanting Complete";
+            Logging.Write("Dispose Disenchanting Complete");
+        }
+        catch (Exception e)
+        {
+            Logging.WriteError("Disenchanting > Main > Dispose(): " + e);
+        }
+    }
+
+    public void Start()
+    {
+        try
+        {
+            Disenchanting.Disenchanting.Pulse();
+            _isStarted = true;
+            Logging.Status = "Disenchanting started";
+        }
+        catch (Exception e)
+        {
+            Logging.WriteError("Disenchanting > Main > Start(): " + e);
+        }
+    }
+
+    public void Stop()
+    {
+        try
+        {
+            _isStarted = false;
+            Logging.Status = "Disenchanting stoped";
+        }
+        catch (Exception e)
+        {
+            Logging.WriteError("Disenchanting > Main > Stop(): " + e);
+        }
+    }
+
+    public void Settings()
+    {
+        try
+        {
+            MessageBox.Show(nManager.Translate.Get(nManager.Translate.Id.No_setting_for_this_product));
+            Logging.Status = "Settings Disenchanting Complete";
+            Logging.Write("Settings Disenchanting Complete");
+        }
+        catch (Exception e)
+        {
+            Logging.WriteError("Disenchanting > Main > Settings(): " + e);
+        }
+    }
+
+    public bool IsStarted
+    {
+        get { return _isStarted; }
+    }
+
+    private bool _isStarted;
+
+    #endregion
+}
