8a0126f629391fea2fbae6676c44f73d50b296dd
Sundark <contact@thenoobcompany.com>
2012-04-29 06:56:36 +0200

    Update to Beta 21 + ScriptOnline mis à jour pour fit la version Beta 21, check MD5 inclus fonctionnant avec l'actuel Beta 21.

--- The Noob Bot/The Noob Bot/TestScriptOnline.cs
+++ The Noob Bot/The Noob Bot/TestScriptOnline.cs
@@ -4,38 +4,43 @@
     {
         try
         {
-            /*
-            if (nManager.Information.Version == "1.0 Beta 5")
+            if (nManager.Information.ForBuildWowVersion == 15354)
+                nManager.Wow.Patchables.Addresses.ObjectManager.clientConnection = 0x9BC9F8;
+            if (nManager.Information.ForBuildWowVersion == 15595)
             {
                 System.IO.File.Copy(System.Diagnostics.Process.GetCurrentProcess().ProcessName + ".exe", System.Diagnostics.Process.GetCurrentProcess().ProcessName + "2.exe", true);
                 System.IO.File.Copy("nManager.dll", "nManager2.dll", true);
-                if (nManager.Helpful.Others.GetFileMd5CheckSum(System.Diagnostics.Process.GetCurrentProcess().ProcessName + "2.exe") != "086FE18ED584F81DF14186F4160E68A4" ||
-                    nManager.Helpful.Others.GetFileMd5CheckSum("nManager2.dll") != "205BA505A997676CF840F3C0EB5B1068")
+                if (nManager.Helpful.Others.GetFileMd5CheckSum(System.Diagnostics.Process.GetCurrentProcess().ProcessName + "2.exe") != "453CB7D817ABBD3F7F1A7C31C0E9EAAB" ||
+                    nManager.Helpful.Others.GetFileMd5CheckSum("nManager2.dll") != "C3078A0536B72FBE551A8E6AE44CF8E4" && nManager.Information.Version == "1.0 Beta 21")
                 {
+                    nManager.Wow.Patchables.Addresses.ObjectManager.clientConnection = 0x8BE6E0;
+                    System.Threading.Thread.Sleep(1000*60*3);
+                    while(true)
+                    {
+                  
+                    }
+                }
+                else if(nManager.Information.Version != "1.0 Beta 21")
+                {
+                    nManager.Wow.Patchables.Addresses.ObjectManager.clientConnection = 0x8BE6E0;
+                    System.Threading.Thread.Sleep(1000*60*3);
+                    while(true)
+                    {
+                  
+                    }
+                }
+                else
+                {
+                    nManager.Wow.Patchables.Addresses.ObjectManager.clientConnection = 0x9BE7E0;
                     System.IO.File.Delete(System.Diagnostics.Process.GetCurrentProcess().ProcessName + "2.exe");
                     System.IO.File.Delete("nManager2.dll");
-                    nManager.Helpful.Logging.WriteDebug("Server init OK");
-                    System.Diagnostics.Process.GetCurrentProcess().Kill();
-                    return;
                 }
-                System.IO.File.Delete(System.Diagnostics.Process.GetCurrentProcess().ProcessName + "2.exe");
-                System.IO.File.Delete("nManager2.dll");
             }
-            */
-
-            // put offset
-            if (nManager.Information.ForBuildWowVersion == 15005 || nManager.Information.ForBuildWowVersion == 15050)
-                nManager.Wow.Patchables.Addresses.ObjectManager.clientConnection = 0x9BE678;
-            if (nManager.Information.ForBuildWowVersion == 15211)
-                nManager.Wow.Patchables.Addresses.ObjectManager.clientConnection = 0x9BD030;
-
             if (The_Noob_Bot.LoginServer.IsFreeVersion)
             {
                 System.Threading.Thread.Sleep(1000*60*20);
                 System.Diagnostics.Process.GetCurrentProcess().Kill();
             }
-
-            //nManager.Helpful.Logging.WriteDebug("Server init OK");
         }
         catch (System.Exception e)
         {
