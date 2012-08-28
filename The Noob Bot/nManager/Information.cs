7ea0a85482059c7f7afc89cc876b917bc239d02f
Sundark <contact@thenoobcompany.com>
2012-08-29 00:48:59 +0200

    Organize the /docs/ folders a bit better TODO: Fix CRLF.

--- The Noob Bot/nManager/Information.cs
+++ The Noob Bot/nManager/Information.cs
@@ -2,8 +2,8 @@
 {
     public static class Information
     {
-        public const string ForWow = "4.3.4";
+        public const string ForWow = "5.0.4";
         public const int ForBuildWowVersion = 16016;
-        public const string Version = "1.1.3";
+        public const string Version = "1.2 Beta";
     }
 }
