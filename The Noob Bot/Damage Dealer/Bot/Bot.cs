fcff427cfa63d6622d71cb793d3d7800f9846f4b
TheNoobCompany <contact@thenoobcompany.com>
2012-09-18 10:05:36 +0200

    Prepare DamageDealer product.
    Add "FightHostileTarget".
    TODO: Check our current target, if hostile, start fight.
    TODO Respect: Never use movement manager.

---
+++ The Noob Bot/Damage Dealer/Bot/Bot.cs
@@ -0,0 +1,64 @@
+using System;
+using nManager.FiniteStateMachine;
+using nManager.Helpful;
+using nManager.Wow.Helpers;
+
+namespace DamageDealer.Bot
+{
+    internal class Bot
+    {
+        private static readonly Engine Fsm = new Engine();
+
+        internal static bool Pulse()
+        {
+            try
+            {
+                // Update spell list
+                SpellManager.UpdateSpellBook();
+
+                // Load CC:
+                CustomClass.LoadCustomClass();
+
+                // FSM
+                Fsm.States.Clear();
+
+                Fsm.AddState(new nManager.Wow.Bot.States.Pause { Priority = 12 });
+                Fsm.AddState(new nManager.Wow.Bot.States.IsAttacked { Priority = 10 });
+                Fsm.AddState(new nManager.Wow.Bot.States.FightHostileTarget { Priority = 9 });
+                Fsm.AddState(new nManager.Wow.Bot.States.Talents {Priority = 3});
+                Fsm.AddState(new nManager.Wow.Bot.States.Idle {Priority = 0});
+
+                Fsm.States.Sort();
+                Fsm.StartEngine(6); // Fsm.StartEngine(25);
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                try
+                {
+                    Dispose();
+                }
+                catch
+                {
+                }
+                Logging.WriteError("Damage Dealer > Bot > Bot  > Pulse(): " + e);
+                return false;
+            }
+        }
+
+        internal static void Dispose()
+        {
+            try
+            {
+                CustomClass.DisposeCustomClass();
+                Fsm.StopEngine();
+                Fight.StopFight();
+            }
+            catch (Exception e)
+            {
+                Logging.WriteError("Damage Dealer > Bot > Bot  > Dispose(): " + e);
+            }
+        }
+    }
+}
---
+++ The Noob Bot/nManager/Wow/Bot/States/FightHostileTarget.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using nManager.FiniteStateMachine;
+using nManager.Helpful;
+using nManager.Wow.Enums;
+using nManager.Wow.Helpers;
+using nManager.Wow.ObjectManager;
+
+namespace nManager.Wow.Bot.States
+{
+    public class FightHostileTarget : State
+    {
+        private WoWUnit _unit;
+
+        public override string DisplayName
+        {
+            get { return "FightHostileTarget"; }
+        }
+
+        public override int Priority
+        {
+            get { return _priority; }
+            set { _priority = value; }
+        }
+
+        private int _priority;
+
+        public override bool NeedToRun
+        {
+            get
+            {
+                if (!Usefuls.InGame || Usefuls.IsLoadingOrConnecting ||
+                    ObjectManager.ObjectManager.Me.IsDeadMe ||
+                    !ObjectManager.ObjectManager.Me.IsValid ||
+                    ObjectManager.ObjectManager.Me.IsMounted ||
+                    !Products.Products.IsStarted)
+                    return false;
+
+                // Check if we have a target, and if this target is hostile or not:
+                _unit = null;
+                /*if (UnitRelation.GetReaction(ObjectManager.ObjectManager.Me.Faction, ObjectManager.ObjectManager.Target.Faction) == Reaction.Hostile)
+                {
+                    _unit = ObjectManager.ObjectManager.Me.Target;
+                    return true;
+                }*/
+                if (_unit != null)
+                    if (_unit.IsValid)
+                        return true;
+
+                return false;
+            }
+        }
+
+        public override List<State> NextStates
+        {
+            get { return new List<State>(); }
+        }
+
+        public override List<State> BeforeStates
+        {
+            get { return new List<State>(); }
+        }
+
+        public override void Run()
+        {
+            MovementManager.StopMove();
+            MovementManager.StopMove();
+            Logging.Write("Currently fighting against " + _unit.Name + " (lvl " + _unit.Level + ")");
+            Fight.StartFight(_unit.Guid);
+            if (_unit.IsDead)
+            {
+                Statistics.Kills++;
+                Thread.Sleep(Usefuls.Latency + 800);
+                while (!ObjectManager.ObjectManager.Me.IsMounted && ObjectManager.ObjectManager.Me.InCombat && ObjectManager.ObjectManager.GetUnitAttackPlayer().Count <= 0)
+                {
+                    Thread.Sleep(10);
+                }
+                Fight.StopFight();
+            }
+        }
+    }
+}
