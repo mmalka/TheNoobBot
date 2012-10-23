ada1a3ae7a9ca206988b75673c04b84f755a0960
TheNoobCompany <contact@thenoobcompany.com>
2012-10-23 11:26:20 +0200

    Allow 5 seconds of jumping before release the jump. (If the Z still not at the right position, it will wait approx 1 second before jumping back in anyway, so it wont break the movement itself, just avoid the "jump forever" bug)

--- The Noob Bot/nManager/Wow/Helpers/LongMove.cs
+++ The Noob Bot/nManager/Wow/Helpers/LongMove.cs
@@ -69,6 +69,7 @@ namespace nManager.Wow.Helpers
                 Point pTemps = ObjectManager.ObjectManager.Me.Position;
 
                 var timerSit = new Helpful.Timer(2500);
+                var jump_timer = new Helpful.Timer(0);
 
                 while (Products.Products.IsStarted && ObjectManager.ObjectManager.Me.IsMounted &&
                        ObjectManager.ObjectManager.Me.Position.DistanceTo(point) > 3.5f && _used && _usedLoop)
@@ -102,6 +103,9 @@ namespace nManager.Wow.Helpers
                                 }
                                 if (TraceLine.TraceLineGo(meTemps, temps))
                                 {
+                                    jump_timer = new Helpful.Timer(5000);
+                                    while(!jump_timer.IsReady)
+                                        MovementManager.StopMoveTo(false);
                                     MovementManager.StopMoveTo(true);
                                 }
                                 // End Stop move to
