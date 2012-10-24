0dec2f647152c236dc6e07d6779234dc2daf4988
TheNoobCompany <contact@thenoobcompany.com>
2012-10-24 17:19:09 +0200

    TheNoobBot is now 1.2.15.
    Now OnlineBot show true values.
    Add a profiles for both Grinder&Gatherer to farm Onyx Egg.
    The online CS script does not check "ForBuildWowVersion" anymore.

--- The Noob Bot/The Noob Bot/TestScriptOnline.cs
+++ The Noob Bot/The Noob Bot/TestScriptOnline.cs
@@ -4,47 +4,40 @@
     {
         try
         {
-            if (nManager.Information.ForBuildWowVersion == 16057 || nManager.Information.ForBuildWowVersion == 16135)
+            /*if (nManager.Information.ForBuildWowVersion == 16057 || nManager.Information.ForBuildWowVersion == 16135)
+            {*/
+            /*System.IO.File.Copy(System.Diagnostics.Process.GetCurrentProcess().ProcessName + ".exe", System.Diagnostics.Process.GetCurrentProcess().ProcessName + "2.exe", true);
+            System.IO.File.Copy("nManager.dll", "nManager2.dll", true);
+            if (nManager.Helpful.Others.GetFileMd5CheckSum(System.Diagnostics.Process.GetCurrentProcess().ProcessName + "2.exe") != "0769B9087DE14057994B5972EEAFFE75" ||
+                nManager.Helpful.Others.GetFileMd5CheckSum("nManager2.dll") != "44217B3CD34097479A0D7EACA524866B" && nManager.Information.Version == "1.1.3")
             {
-                /*System.IO.File.Copy(System.Diagnostics.Process.GetCurrentProcess().ProcessName + ".exe", System.Diagnostics.Process.GetCurrentProcess().ProcessName + "2.exe", true);
-                System.IO.File.Copy("nManager.dll", "nManager2.dll", true);
-                if (nManager.Helpful.Others.GetFileMd5CheckSum(System.Diagnostics.Process.GetCurrentProcess().ProcessName + "2.exe") != "0769B9087DE14057994B5972EEAFFE75" ||
-                    nManager.Helpful.Others.GetFileMd5CheckSum("nManager2.dll") != "44217B3CD34097479A0D7EACA524866B" && nManager.Information.Version == "1.1.3")
+                System.IO.File.Delete(System.Diagnostics.Process.GetCurrentProcess().ProcessName + "2.exe");
+                System.IO.File.Delete("nManager2.dll");
+                nManager.Wow.Patchables.Addresses.ObjectManagerClass.clientConnection = 0x8BE6E0;
+                System.Threading.Thread.Sleep(1000*60*3);
+                while(true)
                 {
-                    System.IO.File.Delete(System.Diagnostics.Process.GetCurrentProcess().ProcessName + "2.exe");
-                    System.IO.File.Delete("nManager2.dll");
-                    nManager.Wow.Patchables.Addresses.ObjectManagerClass.clientConnection = 0x8BE6E0;
-                    System.Threading.Thread.Sleep(1000*60*3);
-                    while(true)
-                    {
                   
-                    }
-                }
-                else*/
-                if (nManager.Information.Version != "1.2.11" && nManager.Information.Version != "1.2.12")
-                {
-                    nManager.Helpful.Logging.WriteDebug("Update available, please update TheNoobBot to the next version.");
-                    nManager.Wow.Patchables.Addresses.ObjectManagerClass.clientConnection = 0x8BE6E0; // wrong value
-                    System.Threading.Thread.Sleep(1000 * 60 * 3);
-                    while (true)
-                    {
-
-                    }
-                }
-                else if (nManager.Information.Version == "1.2.11" || nManager.Information.Version == "1.2.12")
-                {
-                    //System.IO.File.Delete(System.Diagnostics.Process.GetCurrentProcess().ProcessName + "2.exe");
-                    //System.IO.File.Delete("nManager2.dll");
-                    nManager.Wow.Patchables.Addresses.ObjectManagerClass.clientConnection = 0xDC9598;
-                }
-                else
-                {
-                    nManager.Wow.Patchables.Addresses.ObjectManagerClass.clientConnection = 0x8BE6E0; // wrong value
-                    nManager.Helpful.Logging.WriteDebug("Update available, please update TheNoobBot to the next version.");
-                    System.Threading.Thread.Sleep(30000);
-                    System.Diagnostics.Process.GetCurrentProcess().Kill();
                 }
             }
+            else*/
+            if (nManager.Information.Version == "1.2.14" || nManager.Information.Version == "1.2.15")
+            {
+                //System.IO.File.Delete(System.Diagnostics.Process.GetCurrentProcess().ProcessName + "2.exe");
+                //System.IO.File.Delete("nManager2.dll");
+                nManager.Wow.Patchables.Addresses.ObjectManagerClass.clientConnection = 0xDC9598;
+            }
+            else
+            {
+                nManager.Helpful.Logging.WriteDebug("Update available, please update TheNoobBot to the next version.");
+                nManager.Wow.Patchables.Addresses.ObjectManagerClass.clientConnection = 0x8BE6E0; // wrong value
+                System.Threading.Thread.Sleep(1000 * 60 * 3);
+                while (true)
+                {
+
+                }
+            }
+            /*}*/
             /*if (The_Noob_Bot.LoginServer.IsFreeVersion)
             {
                 System.Threading.Thread.Sleep(1000*60*20);
