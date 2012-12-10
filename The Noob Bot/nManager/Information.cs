21d66bda055751ff1b2993c5c4d7782d756e9f88
TheNoobCompany <contact@thenoobcompany.com>
2012-12-10 20:46:45 +0100

    Latest update

--- The Noob Bot/nManager/Information.cs
+++ The Noob Bot/nManager/Information.cs
@@ -3,7 +3,7 @@
     public static class Information
     {
         public const string TargetWowVersion = "5.1.0";
-        public const int TargetWowBuild = 16309;
-        public const string Version = "1.3.4";
+        public const int TargetWowBuild = 16357;
+        public const string Version = "1.3.5";
     }
 }
