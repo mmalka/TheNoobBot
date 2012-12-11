599fb5e7f1316db41881de0fab2143237c0a9a6b
Droidz <snip78@gmail.com>
2012-12-11 15:44:50 +0100

    Update to wow 5.1.0a - 16357

--- The Noob Bot/nManager/Information.cs
+++ The Noob Bot/nManager/Information.cs
@@ -2,7 +2,7 @@
 {
     public static class Information
     {
-        public const string TargetWowVersion = "5.1.0";
+        public const string TargetWowVersion = "5.1.0a";
         public const int TargetWowBuild = 16357;
         public const string Version = "1.3.5";
     }
