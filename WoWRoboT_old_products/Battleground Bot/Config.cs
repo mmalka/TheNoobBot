c1e86bcf750b84ab0be62004a065ee655de47cd5
TheNoobCompany <contact@thenoobcompany.com>
2013-02-09 19:53:08 +0100

    Remove old WoWRobot products examples.

--- WoWRoboT_old_products/Battleground Bot/Config.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Battleground_Bot
-{
-    public static class Config
-    {
-        public static Bot.Bot Bot = new Bot.Bot();
-    }
-}
--- WoWRoboT_old_products/Questing Bot/Bot/Config.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Questing_Bot.Bot
-{
-    public class Config
-    {
-        public static Bot Bot = new Bot();
-    }
-}
