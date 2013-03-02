843f66a31c396e8eb9c6cb4750426a715b53bef4
TheNoobCompany <contact@thenoobcompany.com>
2013-03-02 18:51:49 +0100

    Add Hapiness to PowerType.
    Add some comments to PowerType.
    Note: There is only 15 PowerType in FrameXML\Constants.lua, see http://www.wowpedia.org/PowerType
    ArcaneCharges does not appear on Constants.lua, we need to test it.
    Note: LightForce seems to have been replaced by Chi, and DarkForce seems to be useless for now but not removed, we don't know if it's a Alpha PowerType of the monk, or if it's a placeholder for the future. (The alpha powertype is currently the best guess, as "Light/Dark force" would have been a good Yin & Yang combo, but they seems to have preferred "Chi" ("Qi" - Energy/Vital Power) over the Yin & Yang model (opposite forces))

--- The Noob Bot/nManager/Wow/Enums/WoWPowerType.cs
+++ The Noob Bot/nManager/Wow/Enums/WoWPowerType.cs
@@ -6,18 +6,19 @@
         Rage = 0x1,
         Focus = 0x2,
         Energy = 0x3,
+        Hapiness = 0x4,
         Runes = 0x5,
         RunicPower = 0x6,
         SoulShards = 0x7,
         Eclipse = 0x8,
         HolyPower = 0x9,
-        Alternate = 0xA,
-        DarkForce = 0xB,
-        LightForce = 0xC,
+        Alternate = 0xA, // Alternate bar. (Example: Atramedes Song bar)
+        DarkForce = 0xB, // Previous PowerType of the Monk. Not used anymore.
+        LightForce = 0xC, // Previous PowerType of the Monk. Now called Chi.
         Chi = 0xC,
         ShadowOrbs = 0xD,
         BurningEmbers = 0xE,
         DemonicFury = 0xF,
-        ArcaneCharges = 0x10
+        ArcaneCharges = 0x10, // Not proven to really exist, need testing on Mage.
     };
 }
