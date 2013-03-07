6b2aa19a9bee62c635e7c081a8dde3d840d421e7
TheNoobCompany <contact@thenoobcompany.com>
2013-03-07 13:42:11 +0100

    Update README, with a candidate to replace buggy KeyPress actions. We will get ride of Keybindings issues (mouse button etc).
    Update ClickToMove function, move the logging a bit down, it will avoid spamming it, when the bot is actually not using CTM because x_Codecave are empty.

--- The Noob Bot/nManager/Wow/Helpers/ClickToMove.cs
+++ The Noob Bot/nManager/Wow/Helpers/ClickToMove.cs
@@ -14,9 +14,7 @@ namespace nManager.Wow.Helpers
             {
                 if (x == 0 && y == 0 && z == 0 && guid == 0)
                     return;
-                Logging.WriteDebug("MoveTo(" + x + ", " + y + ", " + z + ", " + guid + ", " + action + ", " + precision +
-                                   ")");
-
+                
                 // Allocate Memory:
                 var posCodecave = Memory.WowMemory.Memory.AllocateMemory(0x4*3);
                 var guidCodecave = Memory.WowMemory.Memory.AllocateMemory(0x8);
@@ -57,7 +55,8 @@ namespace nManager.Wow.Helpers
                     };
 
                 Memory.WowMemory.InjectAndExecute(asm);
-
+                Logging.WriteNavigator("MoveTo(" + x + ", " + y + ", " + z + ", " + guid + ", " + action + ", " + precision +
+                                   ")");
                 Memory.WowMemory.Memory.FreeMemory(posCodecave);
                 Memory.WowMemory.Memory.FreeMemory(guidCodecave);
                 Memory.WowMemory.Memory.FreeMemory(precisionCodecave);
