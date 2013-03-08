9ebea28b40fd5ac10781be0a25ddc0bd76dbb210
TheNoobCompany <contact@thenoobcompany.com>
2013-03-08 16:14:01 +0100

    Revert Idle.cs to its original way of working.
    Add a proper State for PositionTrackingSystem. (Note: not used in any product -yet-)

--- The Noob Bot/nManager/Wow/Bot/States/Idle.cs
+++ The Noob Bot/nManager/Wow/Bot/States/Idle.cs
@@ -1,26 +1,25 @@
 ﻿using System.Collections.Generic;
 using System.Threading;
 using nManager.FiniteStateMachine;
-using nManager.Helpful;
-using nManager.Wow.Helpers;
-using Timer = nManager.Helpful.Timer;
 
 namespace nManager.Wow.Bot.States
 {
     public class Idle : State
     {
-        private readonly Timer _canWrite = new Timer(1000*5);
-
-        public int TimeSleepMs = 60;
-        private string _currPosition;
-        private string _lastPosition;
-
         public override string DisplayName
         {
             get { return "Idle"; }
         }
 
-        public override int Priority { get; set; }
+        public override int Priority
+        {
+            get { return _priority; }
+            set { _priority = value; }
+        }
+
+        private int _priority;
+
+        public int TimeSleepMs = 60;
 
         public override bool NeedToRun
         {
@@ -40,14 +39,6 @@ namespace nManager.Wow.Bot.States
         public override void Run()
         {
             Thread.Sleep(TimeSleepMs);
-            if (!Usefuls.InGame || Usefuls.IsLoadingOrConnecting) return;
-            _currPosition = string.Format("CurrentPositionTracker({0}, {1}, {2}, {3});", ObjectManager.ObjectManager.Me.Position.X, ObjectManager.ObjectManager.Me.Position.Y,
-                                          ObjectManager.ObjectManager.Me.Position.Z, ObjectManager.ObjectManager.Me.Position.Type);
-            if (_currPosition == _lastPosition) return;
-            _lastPosition = _currPosition;
-            if (!_canWrite.IsReady) return;
-            _canWrite.Reset();
-            Logging.WriteFileOnly(_currPosition);
         }
     }
 }
---
+++ The Noob Bot/nManager/Wow/Bot/States/PositionTrackingSystem.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using nManager.FiniteStateMachine;
+using nManager.Helpful;
+using nManager.Wow.Helpers;
+
+namespace nManager.Wow.Bot.States
+{
+    public class PositionTrackingSystem : State
+    {
+        private readonly Timer _canWrite = new Timer(1000*5);
+        private string _currPosition;
+        private string _lastPosition;
+
+        public override string DisplayName
+        {
+            get { return "PositionTrackingSystem"; }
+        }
+
+        public override int Priority { get; set; }
+
+        public override bool NeedToRun
+        {
+            get
+            {
+                if (!Usefuls.InGame || Usefuls.IsLoadingOrConnecting) return false;
+                _currPosition = string.Format("CurrentPositionTracker({0}, {1}, {2}, {3});", ObjectManager.ObjectManager.Me.Position.X, ObjectManager.ObjectManager.Me.Position.Y,
+                                              ObjectManager.ObjectManager.Me.Position.Z, ObjectManager.ObjectManager.Me.Position.Type);
+                if (_currPosition == _lastPosition) return false;
+                _lastPosition = _currPosition;
+                return _canWrite.IsReady;
+            }
+        }
+
+        public override List<State> NextStates
+        {
+            get { return new List<State>(); }
+        }
+
+        public override List<State> BeforeStates
+        {
+            get { return new List<State>(); }
+        }
+
+        public override void Run()
+        {
+            Logging.WriteFileOnly(_currPosition);
+            _canWrite.Reset();
+        }
+    }
+}
