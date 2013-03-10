ac56cc2129f7360dc9e7d273e6a976635e67ab80
TheNoobCompany <contact@thenoobcompany.com>
2013-03-10 01:39:30 +0100

    Add logs over MovementsAction, currently having some trouble with the bot, often going up in the air (forget to stop the ascend, or it even don't work...)

--- The Noob Bot/nManager/Wow/Helpers/MovementsAction.cs
+++ The Noob Bot/nManager/Wow/Helpers/MovementsAction.cs
@@ -1,40 +1,48 @@
-﻿namespace nManager.Wow.Helpers
+﻿using nManager.Helpful;
+
+namespace nManager.Wow.Helpers
 {
     public class MovementsAction
     {
         public static void Jump()
         {
-            Lua.LuaDoString("JumpOrAscendStart();");
-            Lua.LuaDoString("AscendStop();");
+            Ascend(true);
+            Ascend(false);
         }
 
         public static void Ascend(bool start)
         {
+            Logging.WriteFileOnly("Ascend("+ start +")");
             Lua.LuaDoString(start ? "JumpOrAscendStart();" : "AscendStop();");
         }
 
         public static void Descend(bool start)
         {
+            Logging.WriteFileOnly("Descend(" + start + ")");
             Lua.LuaDoString(start ? "SitStandOrDescendStart();" : "DescendStop();");
         }
 
         public static void MoveBackward(bool start)
         {
+            Logging.WriteFileOnly("MoveBackward(" + start + ")");
             Lua.LuaDoString(start ? "MoveBackwardStart();" : "MoveBackwardStop();");
         }
 
         public static void MoveForward(bool start)
         {
+            Logging.WriteFileOnly("MoveForward(" + start + ")");
             Lua.LuaDoString(start ? "MoveForwardStart();" : "MoveForwardStop();");
         }
 
         public static void StrafeLeft(bool start)
         {
+            Logging.WriteFileOnly("StrafeLeft(" + start + ")");
             Lua.LuaDoString(start ? "StrafeLeftStart();" : "StrafeLeftStop();");
         }
 
         public static void StrafeRight(bool start)
         {
+            Logging.WriteFileOnly("StrafeRight(" + start + ")");
             Lua.LuaDoString(start ? "StrafeRightStart();" : "StrafeRightStop();");
         }
     }
