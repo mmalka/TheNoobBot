43e10467655ab5f439f478762a511fef995afe7a
TheNoobCompany <contact@thenoobcompany.com>
2013-03-10 02:27:36 +0100

    Clever way revert of the LUA movements system to Key system. LUA for movements does not work as intended, maybe it will be better to use the Memory functions instead,
    I don't know actually if it's a time to process issue, or just if LUA is not designed to react greatly.

--- The Noob Bot/nManager/Wow/Helpers/MovementsAction.cs
+++ The Noob Bot/nManager/Wow/Helpers/MovementsAction.cs
@@ -4,6 +4,8 @@ namespace nManager.Wow.Helpers
 {
     public class MovementsAction
     {
+        public static bool UseLUAToMove = false; // RECOMMANDED AS HELL until I understand why LUA don't wanna work as intended.
+
         public static void Jump()
         {
             Ascend(true);
@@ -12,38 +14,98 @@ namespace nManager.Wow.Helpers
 
         public static void Ascend(bool start)
         {
-            Logging.WriteFileOnly("Ascend("+ start +")");
-            Lua.LuaDoString(start ? "JumpOrAscendStart();" : "AscendStop();");
+            Logging.WriteFileOnly("Ascend(" + start + ")");
+            if (UseLUAToMove)
+            {
+                Lua.LuaDoString(start ? "JumpOrAscendStart();" : "AscendStop();");
+            }
+            else
+            {
+                if (start)
+                    Keybindings.DownKeybindings(Enums.Keybindings.JUMP);
+                else
+                    Keybindings.UpKeybindings(Enums.Keybindings.JUMP);
+            }
         }
 
         public static void Descend(bool start)
         {
             Logging.WriteFileOnly("Descend(" + start + ")");
-            Lua.LuaDoString(start ? "SitStandOrDescendStart();" : "DescendStop();");
+            if (UseLUAToMove)
+            {
+                Lua.LuaDoString(start ? "SitStandOrDescendStart();" : "DescendStop();");
+            }
+            else
+            {
+                if (start)
+                    Keybindings.DownKeybindings(Enums.Keybindings.SITORSTAND);
+                else
+                    Keybindings.UpKeybindings(Enums.Keybindings.SITORSTAND);
+            }
         }
 
         public static void MoveBackward(bool start)
         {
             Logging.WriteFileOnly("MoveBackward(" + start + ")");
-            Lua.LuaDoString(start ? "MoveBackwardStart();" : "MoveBackwardStop();");
+            if (UseLUAToMove)
+            {
+                Lua.LuaDoString(start ? "MoveBackwardStart();" : "MoveBackwardStop();");
+            }
+            else
+            {
+                if (start)
+                    Keybindings.DownKeybindings(Enums.Keybindings.MOVEBACKWARD);
+                else
+                    Keybindings.UpKeybindings(Enums.Keybindings.MOVEBACKWARD);
+            }
         }
 
         public static void MoveForward(bool start)
         {
             Logging.WriteFileOnly("MoveForward(" + start + ")");
-            Lua.LuaDoString(start ? "MoveForwardStart();" : "MoveForwardStop();");
+            if (UseLUAToMove)
+            {
+                Lua.LuaDoString(start ? "MoveForwardStart();" : "MoveForwardStop();");
+            }
+            else
+            {
+                if (start)
+                    Keybindings.DownKeybindings(Enums.Keybindings.MOVEFORWARD);
+                else
+                    Keybindings.UpKeybindings(Enums.Keybindings.MOVEFORWARD);
+            }
         }
 
         public static void StrafeLeft(bool start)
         {
             Logging.WriteFileOnly("StrafeLeft(" + start + ")");
-            Lua.LuaDoString(start ? "StrafeLeftStart();" : "StrafeLeftStop();");
+            if (UseLUAToMove)
+            {
+                Lua.LuaDoString(start ? "StrafeLeftStart();" : "StrafeLeftStop();");
+            }
+            else
+            {
+                if (start)
+                    Keybindings.DownKeybindings(Enums.Keybindings.STRAFELEFT);
+                else
+                    Keybindings.UpKeybindings(Enums.Keybindings.STRAFELEFT);
+            }
         }
 
         public static void StrafeRight(bool start)
         {
             Logging.WriteFileOnly("StrafeRight(" + start + ")");
-            Lua.LuaDoString(start ? "StrafeRightStart();" : "StrafeRightStop();");
+            if (UseLUAToMove)
+            {
+                Lua.LuaDoString(start ? "StrafeRightStart();" : "StrafeRightStop();");
+            }
+            else
+            {
+                if (start)
+                    Keybindings.DownKeybindings(Enums.Keybindings.STRAFERIGHT);
+                else
+                    Keybindings.UpKeybindings(Enums.Keybindings.STRAFERIGHT);
+            }
         }
     }
 }
