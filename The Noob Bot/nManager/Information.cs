7e18ef899dd0caee0296e800f11b59a2c6390f2a
TheNoobCompany <contact@thenoobcompany.com>
2013-03-15 03:11:30 +0100

    TheNoobBot is now 1.5.3.
    The commit since "Update to 5.2.0.16701." have not been included into the 1.5.3 public release.

--- The Noob Bot/nManager/Information.cs
+++ The Noob Bot/nManager/Information.cs
@@ -3,7 +3,7 @@
     public static class Information
     {
         public const string TargetWowVersion = "5.2.0";
-        public const int TargetWowBuild = 16685;
-        public const string Version = "1.5.2";
+        public const int TargetWowBuild = 16701;
+        public const string Version = "1.5.3";
     }
 }
