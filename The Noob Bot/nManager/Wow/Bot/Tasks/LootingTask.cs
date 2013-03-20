d03df3c63056e2555612c3f9e862128f3d37c251
TheNoobCompany <contact@thenoobcompany.com>
2013-03-20 13:01:28 +0100

    Reduce AoE Looting blacklist timet o 2.5 sec.
    Upgrade range from 10y to 20y. (not official though, no valuable infos on this)

--- The Noob Bot/nManager/Wow/Bot/Tasks/LootingTask.cs
+++ The Noob Bot/nManager/Wow/Bot/Tasks/LootingTask.cs
@@ -92,10 +92,9 @@ namespace nManager.Wow.Bot.Tasks
                                             if (!nManagerSetting.CurrentSetting.ActivateBeastSkinning)
                                             {
                                                 WoWUnit unit = wowUnit;
-                                                foreach (var u in woWUnits.Where(u => u != unit).Where(u => u.Position.DistanceTo(unit.Position) <= 10))
+                                                foreach (var u in woWUnits.Where(u => u != unit).Where(u => u.Position.DistanceTo(unit.Position) <= 20f))
                                                 {
-                                                    nManagerSetting.AddBlackList(u.Guid, 1000*4);
-                                                    // TODO: Find the real WoW AoE Range. Superior of 10 for sure, just blacklist 5 seconds the time the Flag IsLootable disapear.
+                                                    nManagerSetting.AddBlackList(u.Guid, 2500);
                                                 }
                                                 nManagerSetting.AddBlackList(wowUnit.Guid, 1000*60*5);
                                                 break;
