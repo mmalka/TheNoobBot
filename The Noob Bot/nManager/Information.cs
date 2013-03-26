a7c1c8b49a10a95fafcc9da7fd4b7287da1ef606
TheNoobCompany <contact@thenoobcompany.com>
2013-03-26 05:39:11 +0100

    Update TheNoobBot to 1.5.9.
    TheNoobBot now support WoW 5.2.0.16769.

--- The Noob Bot/nManager/Information.cs
+++ The Noob Bot/nManager/Information.cs
@@ -3,7 +3,7 @@
     public static class Information
     {
         public const string TargetWowVersion = "5.2.0";
-        public const int TargetWowBuild = 16760;
-        public const string Version = "1.5.8";
+        public const int TargetWowBuild = 16769;
+        public const string Version = "1.5.9";
     }
 }
