95191bbacdda986634b2a69ad0bb7c99f6c40f13
Neo2003 <Neo.2003@Hotmail.fr>
2013-05-07 18:14:22 +0200

    New unstuck jump/dismount attempts at top priority I use this code for several months and it helps a lot I think, especialy during low level quests to jump over various chairs/walls...
    Signed-off-by: Neo2003 <Neo.2003@Hotmail.fr>

--- The Noob Bot/nManager/Wow/Helpers/MovementManager.cs
+++ The Noob Bot/nManager/Wow/Helpers/MovementManager.cs
@@ -29,6 +29,8 @@ namespace nManager.Wow.Helpers
         private static bool _first;
         private static int _lastNbStuck;
         private static bool _farm;
+        private static Point _jumpOverAttempt = new Point();
+        private static Point _distmountAttempt = new Point();
 
         private static int _currentTargetedPoint;
         // let's remember where we are instead of searching point and doing mess
@@ -557,9 +559,51 @@ namespace nManager.Wow.Helpers
                         Logging.WriteDebug("UnStuck - StuckCount updated, new value: " + StuckCount + ".");
                         return;
                     }
+                    Logging.WriteDebug("UnStuck - We are currently mounted.");
+                    MovementsAction.Ascend(true);
+                    Thread.Sleep(Others.Random(500, 1000));
+                    MovementsAction.Ascend(false);
+                    Logging.WriteDebug("UnStuck - Jump attempt done.");
                 }
                 Statistics.Stucks++;
                 Logging.WriteNavigator("UnStuck - Non-flying UnStuck in progress.");
+
+                // Simply try to jump over or dismount if needed
+                if (_distmountAttempt.DistanceTo(ObjectManager.ObjectManager.Me.Position) > 3)
+                {
+                    float dx = 1.5f * (float)CSharpMath.Cos(ObjectManager.ObjectManager.Me.Rotation);
+                    float dy = 1.5f * (float)CSharpMath.Sin(ObjectManager.ObjectManager.Me.Rotation);
+                    Point inFront = new Point(ObjectManager.ObjectManager.Me.Position.X + dx, ObjectManager.ObjectManager.Me.Position.Y + dy, ObjectManager.ObjectManager.Me.Position.Z + 0.8f);
+                    _distmountAttempt = new Point(ObjectManager.ObjectManager.Me.Position.X, ObjectManager.ObjectManager.Me.Position.Y, ObjectManager.ObjectManager.Me.Position.Z + 0.8f);
+                    if (ObjectManager.ObjectManager.Me.IsMounted && !nManager.Wow.Helpers.TraceLine.TraceLineGo(_distmountAttempt, inFront, CGWorldFrameHitFlags.HitTestAll))
+                    {
+                        Logging.WriteNavigator("UnStuck - Dismounting.");
+                        Usefuls.DisMount();
+                        IsUnStuck = false;
+                        StuckCount++;
+                        return;
+                    }
+                    _jumpOverAttempt = new Point(ObjectManager.ObjectManager.Me.Position.X, ObjectManager.ObjectManager.Me.Position.Y, ObjectManager.ObjectManager.Me.Position.Z + 1.5f);
+                    inFront = new Point(ObjectManager.ObjectManager.Me.Position.X + dx, ObjectManager.ObjectManager.Me.Position.Y + dy, ObjectManager.ObjectManager.Me.Position.Z + 1.5f);
+                    if (!nManager.Wow.Helpers.TraceLine.TraceLineGo(_jumpOverAttempt, inFront, CGWorldFrameHitFlags.HitTestAll))
+                    {
+                        Logging.WriteNavigator("UnStuck - Jumping over.");
+                        MovementsAction.Ascend(true);
+                        MovementsAction.MoveForward(true);
+                        Thread.Sleep(75);
+                        MovementsAction.Ascend(false);
+                        MovementsAction.MoveForward(false);
+                        Thread.Sleep(350);
+                        MovementsAction.Ascend(true);
+                        MovementsAction.MoveForward(true);
+                        Thread.Sleep(75);
+                        MovementsAction.Ascend(false);
+                        MovementsAction.MoveForward(false);
+                        IsUnStuck = false;
+                        StuckCount++;
+                        return;
+                    }
+                }
 
                 var lastPost = new Point(ObjectManager.ObjectManager.Me.Position);
                 Logging.WriteDebug("UnStuck - lastPost = " + lastPost);
