ba1747e6a316887b25855b2558fa8d3b94a076c7
TheNoobCompany <contact@thenoobcompany.com>
2013-05-21 15:59:47 +0200

    Update TheNoobBot for 1.6.4.

--- The Noob Bot/nManager/Information.cs
+++ The Noob Bot/nManager/Information.cs
@@ -3,7 +3,7 @@
     public static class Information
     {
         public const string TargetWowVersion = "5.3.0";
-        public const int TargetWowBuild = 16965;
-        public const string Version = "1.6.3"; // next = 1.6.4
+        public const int TargetWowBuild = 16977;
+        public const string Version = "DevVersionRestrict"; // next = 1.6.5
     }
 }
