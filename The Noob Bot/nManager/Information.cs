45e56de52f056da8a1df2fd9b84d5561f68f6489
TheNoobCompany <contact@thenoobcompany.com>
2013-05-20 21:11:32 -0700

    Forgot to change TargetWowVersion. :) Finish !

--- The Noob Bot/nManager/Information.cs
+++ The Noob Bot/nManager/Information.cs
@@ -2,7 +2,7 @@
 {
     public static class Information
     {
-        public const string TargetWowVersion = "5.2.0";
+        public const string TargetWowVersion = "5.3.0";
         public const int TargetWowBuild = 16965;
         public const string Version = "1.6.3"; // next = 1.6.4
     }
