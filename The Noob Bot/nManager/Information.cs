c5184402fd7ec998e65dd82bf0f200480a01ca73
TheNoobCompany <contact@thenoobcompany.com>
2013-09-11 23:08:50 +0300

    Change the next version comment by a current version comment. Prevent error in Commit title about updated version, and more logical as we don't know at time T if we are going to jump from major version number or not.

--- The Noob Bot/nManager/Information.cs
+++ The Noob Bot/nManager/Information.cs
@@ -4,6 +4,6 @@
     {
         public const string TargetWowVersion = "5.3.0";
         public const int TargetWowBuild = 17359;
-        public const string Version = "DevVersionRestrict"; // next = 1.8.3
+        public const string Version = "DevVersionRestrict"; // current = 1.8.2
     }
 }
