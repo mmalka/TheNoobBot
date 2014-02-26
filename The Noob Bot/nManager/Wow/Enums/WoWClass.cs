70f66200367454123835a7463c9b0bf4e8f11b9f
TheNoobCompany <contact@thenoobcompany.com>
2014-02-26 10:49:45 +0200

    Fix "Only CLASS" showing by turning values back to normal decimals instead of hexadecimal.
    Fix a runtime error in showing the class.
    Now show MinLevel instead of QuestLevel as it's definitly a more accurate value to use here.

--- The Noob Bot/nManager/Wow/Enums/WoWClass.cs
+++ The Noob Bot/nManager/Wow/Enums/WoWClass.cs
@@ -16,19 +16,18 @@
         Druid = 11,
     }
 
-    public enum WoWClassMask : uint
+    public enum WoWClassMask
     {
-        None = 0x0,
-        Warrior = 0x1,
-        Paladin = 0x2,
-        Hunter = 0x4,
-        Rogue = 0x8,
-        Priest = 0x16,
-        DeathKnight = 0x32,
-        Shaman = 0x64,
-        Mage = 0x128,
-        Warlock = 0x256,
-        Monk = 0x512,
-        Druid = 0x1024,
+        Warrior = 1,
+        Paladin = 2,
+        Hunter = 4,
+        Rogue = 8,
+        Priest = 16,
+        DeathKnight = 32,
+        Shaman = 64,
+        Mage = 128,
+        Warlock = 256,
+        Monk = 512,
+        Druid = 1024,
     }
 }
