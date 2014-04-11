3e2f7b912fe8b2912b991fde137b42db22ffbf47
Neo2003 <Neo.2003@Hotmail.fr>
2014-04-11 13:57:40 +0200

    Don't serialize default empty Npc and BlackListRadius in Gatherer profiles
    Signed-off-by: Neo2003 <Neo.2003@Hotmail.fr>

--- The Noob Bot/Gatherer/GathererProfile.cs
+++ The Noob Bot/Gatherer/GathererProfile.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using nManager.Wow.Class;
+using System.ComponentModel;
 
 namespace Gatherer
 {
@@ -10,8 +11,10 @@ namespace Gatherer
         public List<Point> Points = new List<Point>();
 
         public List<Npc> Npc = new List<Npc>();
+        public bool ShouldSerializeNpc() { return Npc != null && Npc.Count > 0; }
 
         public List<GathererBlackListRadius> BlackListRadius = new List<GathererBlackListRadius>();
+        public bool ShouldSerializeBlackListRadius() { return BlackListRadius != null && BlackListRadius.Count > 0; }
     }
 
     [Serializable]
