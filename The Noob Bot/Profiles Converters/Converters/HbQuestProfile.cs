7e4319af63c4d14371279d7fe9e5ac57940a10d9
TheNoobCompany <contact@thenoobcompany.com>
2014-04-12 01:46:02 +0300

    Import a WIP of HBQuestingProfile class.

---
+++ The Noob Bot/Profiles Converters/Converters/HbQuestProfile.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Profiles_Converters.Converters
+{
+    public class HBProfile
+    {
+        public string Name = "ProfileTest1";
+        public int MinLevel = 1;
+        public int MaxLevel = 5;
+        public double MinDurability = 0.3;
+        public int MinFreeBagSlots = 3;
+
+        public bool SellGrey = true;
+        public bool SellWhite = false;
+        public bool SellGreen = false;
+        public bool SellBlue = false;
+        public bool SellPurple = false;
+
+        public bool MailGrey = false;
+        public bool MailWhite = false;
+        public bool MailGreen = false;
+        public bool MailBlue = false;
+        public bool MailPurple = false;
+
+        public List<Vendor> Vendors = new List<Vendor>();
+
+        public List<Mailbox> Mailboxes = new List<Mailbox>();
+
+        public List<Blackspot> Blackspots = new List<Blackspot>();
+
+        public List<Mob> AvoidMobs = new List<Mob>();
+
+        [XmlElement("Quest")]
+        //[XmlElement(IsNullable = false)] // Breaks serialize
+        public List<QuestTemplate> Quest { get; set; }
+
+        [Serializable]
+        public class Vendor
+        {
+            //[DefaultValue("")]
+            [XmlAttribute(AttributeName = "Name")] public string Name = "";
+            //[DefaultValue(0)]
+            [XmlAttribute(AttributeName = "Id")] public int Id = 0;
+            //[DefaultValue("")]
+            [XmlAttribute(AttributeName = "Type")] public string Type = "";
+            //[DefaultValue(0)]
+            [XmlAttribute(AttributeName = "X")] public double X = 0;
+            //[DefaultValue(0)]
+            [XmlAttribute(AttributeName = "Y")] public double Y = 0;
+            //[DefaultValue(0)]
+            [XmlAttribute(AttributeName = "Z")] public double Z = 0;
+        }
+
+        [Serializable]
+        public class Mailbox
+        {
+            //[DefaultValue(0)]
+            [XmlAttribute(AttributeName = "X")] public double X = 0;
+            //[DefaultValue(0)]
+            [XmlAttribute(AttributeName = "Y")] public double Y = 0;
+            //[DefaultValue(0)]
+            [XmlAttribute(AttributeName = "Z")] public double Z = 0;
+        }
+
+        [Serializable]
+        public class Blackspot
+        {
+            //[DefaultValue("")]
+            [XmlAttribute(AttributeName = "Name")] public string Name = "";
+            //[DefaultValue(0)]
+            [XmlAttribute(AttributeName = "X")] public double X = 0;
+            //[DefaultValue(0)]
+            [XmlAttribute(AttributeName = "Y")] public double Y = 0;
+            //[DefaultValue(0)]
+            [XmlAttribute(AttributeName = "Z")] public double Z = 0;
+            //[DefaultValue(1)]
+            [XmlAttribute(AttributeName = "Radius")] public int Radius = 1;
+            //[DefaultValue(0)]
+            [XmlAttribute(AttributeName = "Height")] public double Height = 5;
+        }
+
+        [Serializable]
+        public class Hotspot
+        {
+            //[DefaultValue(0)]
+            [XmlAttribute(AttributeName = "X")] public double X = 0;
+            //[DefaultValue(0)]
+            [XmlAttribute(AttributeName = "Y")] public double Y = 0;
+            //[DefaultValue(0)]
+            [XmlAttribute(AttributeName = "Z")] public double Z = 0;
+        }
+
+        [Serializable]
+        public class Mob
+        {
+            //[DefaultValue("")]
+            [XmlAttribute(AttributeName = "Name")] public string Name = "";
+            //[DefaultValue(0)]
+            [XmlAttribute(AttributeName = "Id")] public int Id = 0;
+        }
+
+        [Serializable]
+        public class QuestTemplate
+        {
+            [XmlAttribute(AttributeName = "Id")] public int Id = 0;
+            [XmlAttribute(AttributeName = "Name")] public string Name = "";
+
+            [XmlElement("Objective")]
+            //[XmlElement(IsNullable = false)] // Breaks serialize
+            public List<QuestObjective> Objective { get; set; }
+        }
+
+        [Serializable]
+        public class QuestObjective
+        {
+            public List<Hotspot> Hotspots = new List<Hotspot>();
+            [XmlAttribute(AttributeName = "Type")] public ObjectiveType Type = ObjectiveType.None;
+            [XmlAttribute(AttributeName = "MobId")] public int MobId = 0;
+            [XmlAttribute(AttributeName = "Name")] public int KillCount = 0;
+        }
+
+        [Serializable]
+        public enum ObjectiveType
+        {
+            None,
+            ApplyBuff,
+            BuyItem,
+            EjectVehicle,
+            EquipItem,
+            InteractWith,
+            KillMob,
+            MoveTo,
+            PickUpObject,
+            PickUpQuest,
+            PressKey,
+            UseItem,
+            TurnInQuest,
+            UseFlightPath,
+            UseItemAOE,
+            UseRuneForge,
+            UseSpell,
+            UseSpellAOE,
+            UseVehicle,
+            Wait,
+        }
+    }
+}
--- The Noob Bot/Profiles Converters/Main.cs
+++ The Noob Bot/Profiles Converters/Main.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 using Profiles_Converters;
 using nManager.Helpful;
 using nManager.Products;
+using Profiles_Converters.Converters;
 
 public class Main : IProduct
 {
@@ -39,6 +41,106 @@ public class Main : IProduct
     {
         try
         {
+            /*HBProfile hbQuest = new HBProfile();
+            hbQuest.Vendors = new List<HBProfile.Vendor>
+            {
+                new HBProfile.Vendor {Id = 1, Name = "Vendor1", Type = "Food", X = 15, Y = -30, Z = 45.15}
+            };
+            hbQuest.Mailboxes = new List<HBProfile.Mailbox>
+            {
+                new HBProfile.Mailbox {X = 10, Y = 20.10, Z = 30.20}
+            };
+            hbQuest.Blackspots = new List<HBProfile.Blackspot>
+            {
+                new HBProfile.Blackspot
+                {
+                    Name = "Blackspot1",
+                    X = -1,
+                    Y = -1.2,
+                    Z = 1.3,
+                    Radius = 10,
+                    Height = 20
+                },
+                new HBProfile.Blackspot
+                {
+                    Name = "Blackspot2",
+                    X = 1,
+                    Y = 1.2,
+                    Z = -1.3,
+                    Radius = 10,
+                    Height = 20
+                },
+            };
+            hbQuest.AvoidMobs = new List<HBProfile.Mob>()
+            {
+                new HBProfile.Mob
+                {
+                    Id = 1,
+                    Name = "Mob1"
+                }
+            };
+            hbQuest.Quest = new List<HBProfile.QuestTemplate>();
+            var objectiveQuest1 = new List<HBProfile.QuestObjective>();
+            objectiveQuest1.Add(new HBProfile.QuestObjective()
+            {
+                Hotspots = new List<HBProfile.Hotspot>
+                {
+                    new HBProfile.Hotspot
+                    {
+                        X = 10,
+                        Y = 10,
+                        Z = 20
+                    },
+                },
+                KillCount = 20,
+                MobId = 100,
+                Type = HBProfile.ObjectiveType.ApplyBuff
+            });
+            objectiveQuest1.Add(new HBProfile.QuestObjective()
+            {
+                Hotspots = new List<HBProfile.Hotspot>
+                {
+                    new HBProfile.Hotspot
+                    {
+                        X = 20,
+                        Y = 20,
+                        Z = 10
+                    },
+                },
+                KillCount = 20,
+                MobId = 200,
+                Type = HBProfile.ObjectiveType.KillMob
+            });
+            var objectiveQuest2 = new List<HBProfile.QuestObjective>();
+            objectiveQuest2.Add(new HBProfile.QuestObjective()
+            {
+                Hotspots = new List<HBProfile.Hotspot>
+                {
+                    new HBProfile.Hotspot
+                    {
+                        X = 20,
+                        Y = 20,
+                        Z = 10
+                    },
+                },
+                KillCount = 20,
+                MobId = 200,
+                Type = HBProfile.ObjectiveType.KillMob
+            });
+            hbQuest.Quest.Add(new HBProfile.QuestTemplate
+            {
+                Id = 32,
+                Name = "Quest1",
+                Objective = objectiveQuest1,
+            });
+            hbQuest.Quest.Add(
+                new HBProfile.QuestTemplate
+                {
+                    Id = 23,
+                    Name = "Quest2",
+                    Objective = objectiveQuest2
+                });
+            XmlSerializer.Serialize(Application.StartupPath + @"\test.xml", hbQuest);*/
             formMain = new formMain();
             formMain.Show();
             _isStarted = true;
