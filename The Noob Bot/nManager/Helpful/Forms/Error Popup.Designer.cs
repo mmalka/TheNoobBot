5fe36544f0ff9845d7e03605f4747044659c67cb
TheNoobCompany <contact@thenoobcompany.com>
2014-04-11 23:24:58 +0300

    Update ErrorPopup to the new design. (see: http://i.imgur.com/yPlzFmz.png)

--- The Noob Bot/nManager/Helpful/Forms/Error Popup.Designer.cs
+++ The Noob Bot/nManager/Helpful/Forms/Error Popup.Designer.cs
@@ -29,44 +29,84 @@ namespace nManager.Helpful.Forms
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ErrorPopup));
-            this.OkButton = new DevComponents.DotNetBar.ButtonX();
             this.ErrorDescription = new System.Windows.Forms.Label();
+            this.Controlbar = new System.Windows.Forms.PictureBox();
+            this.Logo = new System.Windows.Forms.PictureBox();
+            this.CloseButton = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Controlbar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CloseButton)).BeginInit();
             this.SuspendLayout();
-            // 
-            // OkButton
-            // 
-            this.OkButton.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
-            this.OkButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.OkButton.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
-            this.OkButton.Location = new System.Drawing.Point(97, 93);
-            this.OkButton.Name = "OkButton";
-            this.OkButton.Size = new System.Drawing.Size(115, 23);
-            this.OkButton.TabIndex = 1;
-            this.OkButton.Text = "Ok";
-            this.OkButton.Click += new System.EventHandler(this.OKButton_Click);
             // 
             // ErrorDescription
             // 
-            this.ErrorDescription.AutoSize = true;
             this.ErrorDescription.BackColor = System.Drawing.Color.White;
-            this.ErrorDescription.ForeColor = System.Drawing.Color.Black;
-            this.ErrorDescription.Location = new System.Drawing.Point(12, 9);
-            this.ErrorDescription.MaximumSize = new System.Drawing.Size(291, 0);
+            this.ErrorDescription.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ErrorDescription.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
+            this.ErrorDescription.Location = new System.Drawing.Point(7, 28);
+            this.ErrorDescription.MaximumSize = new System.Drawing.Size(306, 165);
+            this.ErrorDescription.MinimumSize = new System.Drawing.Size(306, 165);
             this.ErrorDescription.Name = "ErrorDescription";
-            this.ErrorDescription.Size = new System.Drawing.Size(93, 13);
+            this.ErrorDescription.Size = new System.Drawing.Size(306, 165);
             this.ErrorDescription.TabIndex = 3;
             this.ErrorDescription.Text = "Error description";
+            this.ErrorDescription.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Controlbar
+            // 
+            this.Controlbar.ErrorImage = null;
+            this.Controlbar.Image = ((System.Drawing.Image)(resources.GetObject("Controlbar.Image")));
+            this.Controlbar.InitialImage = null;
+            this.Controlbar.Location = new System.Drawing.Point(0, 0);
+            this.Controlbar.Name = "Controlbar";
+            this.Controlbar.Size = new System.Drawing.Size(320, 22);
+            this.Controlbar.TabIndex = 4;
+            this.Controlbar.TabStop = false;
+            this.Controlbar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainFormMouseDown);
+            this.Controlbar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainFormMouseMove);
+            this.Controlbar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MainFormMouseUp);
+            // 
+            // Logo
+            // 
+            this.Logo.BackColor = System.Drawing.Color.Transparent;
+            this.Logo.Image = ((System.Drawing.Image)(resources.GetObject("Logo.Image")));
+            this.Logo.Location = new System.Drawing.Point(3, 3);
+            this.Logo.Name = "Logo";
+            this.Logo.Size = new System.Drawing.Size(15, 16);
+            this.Logo.TabIndex = 5;
+            this.Logo.TabStop = false;
+            this.Logo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainFormMouseDown);
+            this.Logo.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainFormMouseMove);
+            this.Logo.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MainFormMouseUp);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.ErrorImage = null;
+            this.CloseButton.Image = global::nManager.Properties.Resources.close_button;
+            this.CloseButton.InitialImage = null;
+            this.CloseButton.Location = new System.Drawing.Point(302, 3);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(13, 14);
+            this.CloseButton.TabIndex = 6;
+            this.CloseButton.TabStop = false;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            this.CloseButton.MouseEnter += new System.EventHandler(this.CloseButton_MouseEnter);
+            this.CloseButton.MouseLeave += new System.EventHandler(this.CloseButton_MouseLeave);
             // 
             // ErrorPopup
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(306, 128);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(232)))), ((int)(((byte)(232)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(320, 200);
             this.ControlBox = false;
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.Logo);
+            this.Controls.Add(this.Controlbar);
             this.Controls.Add(this.ErrorDescription);
-            this.Controls.Add(this.OkButton);
             this.DoubleBuffered = true;
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -76,14 +116,18 @@ namespace nManager.Helpful.Forms
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Error";
             this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.Controlbar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CloseButton)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private DevComponents.DotNetBar.ButtonX OkButton;
         private System.Windows.Forms.Label ErrorDescription;
+        private System.Windows.Forms.PictureBox Controlbar;
+        private System.Windows.Forms.PictureBox Logo;
+        private System.Windows.Forms.PictureBox CloseButton;
     }
 }
--- The Noob Bot/nManager/Helpful/Forms/Error Popup.cs
+++ The Noob Bot/nManager/Helpful/Forms/Error Popup.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Drawing;
+using System.Windows.Forms;
+using nManager.Properties;
 
 namespace nManager.Helpful.Forms
 {
-    public partial class ErrorPopup : DevComponents.DotNetBar.Metro.MetroForm
+    public partial class ErrorPopup : Form
     {
+        private bool _flagClick;
+        private int _positionInitialeX;
+        private int _positionInitialeY;
+
         public ErrorPopup(string errorMessage)
         {
             InitializeComponent();
@@ -12,9 +19,39 @@ namespace nManager.Helpful.Forms
                 TopMost = true;
         }
 
-        private void OKButton_Click(object sender, EventArgs e)
+        private void CloseButton_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void CloseButton_MouseEnter(object sender, EventArgs e)
+        {
+            CloseButton.Image = Resources.close_buttonG;
+        }
+
+        private void CloseButton_MouseLeave(object sender, EventArgs e)
+        {
+            CloseButton.Image = Resources.close_button;
+        }
+
+        private void MainFormMouseDown(object sender, MouseEventArgs e)
+        {
+            _flagClick = true;
+            _positionInitialeX = e.X;
+            _positionInitialeY = e.Y;
+        }
+
+        private void MainFormMouseUp(object sender, MouseEventArgs e)
+        {
+            _flagClick = false;
+        }
+        
+        private void MainFormMouseMove(object sender, MouseEventArgs e)
+        {
+            if (_flagClick)
+            {
+                Location = new Point(Left + (e.X - _positionInitialeX), Top + (e.Y - _positionInitialeY));
+            }
         }
     }
 }
