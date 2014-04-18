b7fccc6d698cc5a700a245ebde806ae6b171e455
TheNoobCompany <contact@thenoobcompany.com>
2014-04-18 16:23:27 +0300

    import some more code for deserialize:serialize. to be commented for an eventual update

--- The Noob Bot/Profiles Converters/Main.cs
+++ The Noob Bot/Profiles Converters/Main.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using nManager;
 using nManager.Helpful;
-using Profiles_Converters;
 using nManager.Products;
+using nManager.Wow.Class;
+using Profiles_Converters;
 using Profiles_Converters.Converters;
 using Quester.Profile;
 using Quest = Profiles_Converters.Converters.Quest;
@@ -12,6 +15,7 @@ public class Main : IProduct
 {
     #region IProduct Members
 
+    private bool _isStarted;
     private MainForm formMain;
 
     public void Initialize()
@@ -43,23 +47,23 @@ public class Main : IProduct
     {
         try
         {
-            /*var hbProfile = XmlSerializer.Deserialize<HBProfile>(Application.StartupPath + @"\[H - Quest] 85-86 The Jade Forest [Kick].xml");
+            var hbProfile = XmlSerializer.Deserialize<HBProfile>(Application.StartupPath + @"\[H - Quest] 85-86 The Jade Forest [Kick].xml");
             if (hbProfile.Items == null || !hbProfile.Items.Any())
             {
                 return;
             }
-            var count = hbProfile.Items.Length;
+            int count = hbProfile.Items.Length;
             if (count > hbProfile.ItemsElementName.Length)
                 count = hbProfile.ItemsElementName.Length;
-            var machin = new Quester.Profile.Quest();
+            var tnbProfile = new QuesterProfile();
             for (int i = 0; i < count; i++)
             {
-                var name = hbProfile.ItemsElementName[i];
-                var value = hbProfile.Items[i];
+                ItemsChoiceType2 name = hbProfile.ItemsElementName[i];
+                object value = hbProfile.Items[i];
                 Logging.Write(name + ": " + value);
                 if (value.ToString().Contains("Profiles_Converters.Converters"))
                 {
-                    if (value is Vendors)
+                    /*if (value is Vendors)
                     {
                         Vendors vendors = value as Vendors;
                         foreach (var vendor in vendors.Items)
@@ -70,21 +74,215 @@ public class Main : IProduct
                     else if (value is Mailboxes)
                     {
                         Mailboxes mailboxes = value as Mailboxes;
+                        foreach (var mailbox in mailboxes.Items)
+                        {
+                            Logging.Write(mailbox.Nav + ";" + mailbox.X + ";" + mailbox.Y + ";" + mailbox.Z);
+                        }
                     }
-                    else if (value is Blackspots)
+                    else */
+                    if (value is Blackspots)
                     {
-                        Blackspots blackspots = value as Blackspots;
+                        var blackspots = value as Blackspots;
+                        foreach (blackspotType blackspot in blackspots.Blackspot)
+                        {
+                            tnbProfile.Blackspots.Add(new QuesterBlacklistRadius
+                            {
+                                Position = new Point {X = blackspot.X, Y = blackspot.Y, Z = blackspot.Z, Type = ""},
+                                Radius = blackspot.Radius
+                            });
+                            //Logging.Write(blackspot.X + ";" + blackspot.Y + ";" + blackspot.Z + ";" + blackspot.Radius + ";" + blackspot.Height);
+                        }
                     }
-                    else if (value is AvoidMobs)
+                        // avoidmob dosn't gather a valid Entry yet.
+                        /*else if (value is AvoidMobs)
                     {
                         AvoidMobs avoidMobs = value as AvoidMobs;
-                    }
-                    else if (value is Quest)
+                        foreach (var avoidMob in avoidMobs.Items)
+                        {
+                            tnbProfile.AvoidMobs.Add(new Npc()
+                            {
+                                Entry = Others.ToInt32(avoidMob.Entry), Name = avoidMob.Name
+                            });
+                            //Logging.Write(avoidMob.Entry + ";" + avoidMob.Name);
+                        }
+                    }*/
+                    else if (value is Quest && (value as Quest).Id != null) // if id = null, then we are parsing the QuestOrder.
                     {
-                        Quest quest = value as Quest;
+                        var quest = value as Quest;
+                        var tmpQuest = new Quester.Profile.Quest();
+                        tmpQuest.Id = quest.Id != null ? Others.ToInt32(quest.Id) : 0;
+                        tmpQuest.Name = quest.Name;
+                        Logging.Write(quest.Id + ";" + quest.Name);
+                        if (quest.Items.Length <= 0)
+                            return;
+                        foreach (object qContent in quest.Items)
+                        {
+                            if (qContent is objectiveMetaType)
+                            {
+                                var objective = qContent as objectiveMetaType;
+                                if (objective.Type == objectiveTypeType.KillMob)
+                                {
+                                    int killCount = objective.KillCount != null ? Convert.ToInt32(objective.KillCount) : 0;
+                                    int mobId = objective.MobId != null ? Convert.ToInt32(objective.MobId) : 0;
+                                    var hotspotsList = new List<Point>();
+                                    if (objective.Items == null)
+                                        return;
+                                    for (int i2 = 0; i2 < objective.Items.Length; i2++)
+                                    {
+                                        if (!(objective.Items[i2] is Hotspots) && objective.ItemsElementName[i2] == ItemsChoiceType3.Hotspots)
+                                            continue;
+                                        var o = objective.Items[i2] as Hotspots;
+                                        if (o == null || o.Hotspot == null)
+                                            continue;
+                                        foreach (point3dType hot in o.Hotspot)
+                                        {
+                                            var tnbPoint = new Point {X = hot.X, Y = hot.Y, Z = hot.Z};
+                                            hotspotsList.Add(tnbPoint);
+                                        }
+                                    }
+                                    var tmpObjective = new QuestObjective
+                                    {
+                                        Count = killCount,
+                                        Entry = new List<int> {mobId,},
+                                        Objective = Objective.KillMob,
+                                        Hotspots = hotspotsList,
+                                    };
+
+                                    tmpQuest.Objectives.Add(tmpObjective);
+                                    tnbProfile.Quests.Add(tmpQuest);
+                                }
+                                else if (objective.Type == objectiveTypeType.CollectItem)
+                                {
+                                    int collectCount = objective.CollectCount != null ? Convert.ToInt32(objective.CollectCount) : 0;
+                                    int collectItemId = objective.ItemId != null ? Convert.ToInt32(objective.ItemId) : 0;
+                                    var hotspotsList = new List<Point>();
+                                    if (objective.Items == null)
+                                        return;
+                                    var mobList = new List<int>();
+                                    for (int i2 = 0; i2 < objective.Items.Length; i2++)
+                                    {
+                                        if (objective.Items[i2] == null)
+                                            continue;
+                                        if (objective.Items[i2] is Hotspots && objective.ItemsElementName[i2] == ItemsChoiceType3.Hotspots)
+                                        {
+                                            var o = objective.Items[i2] as Hotspots;
+                                            if (o != null && o.Hotspot == null)
+                                                continue;
+                                            foreach (point3dType hot in o.Hotspot)
+                                            {
+                                                var tnbPoint = new Point {X = hot.X, Y = hot.Y, Z = hot.Z};
+                                                hotspotsList.Add(tnbPoint);
+                                            }
+                                        }
+                                        else if (objective.Items[i2] is CollectFrom || objective.ItemsElementName[i2] != ItemsChoiceType3.CollectFrom)
+                                        {
+                                            var o = objective.Items[i2] as CollectFrom;
+                                            if (o == null || o.Items == null || o.Items.Length <= 0)
+                                                continue;
+                                            foreach (object mob in o.Items)
+                                            {
+                                                if (mob is mobObjectiveType)
+                                                {
+                                                    var mo = mob as mobObjectiveType;
+                                                    if (mo.Id != null)
+                                                        mobList.Add(Others.ToInt32(mo.Id));
+                                                }
+                                                else 
+                                                    return;
+                                            }
+                                        }
+                                        var tmpObjective = new QuestObjective
+                                        {
+                                            CollectCount = collectCount,
+                                            CollectItemId = collectItemId,
+                                            Entry = new List<int>(),
+                                            Objective = Objective.KillMob,
+                                            Hotspots = hotspotsList,
+                                        };
+                                    tmpObjective.Entry.AddRange(mobList);
+                                        tmpQuest.Objectives.Add(tmpObjective);
+                                        tnbProfile.Quests.Add(tmpQuest);
+                                    }
+                                }
+                                /*
+                                            else
+                                            {
+                                                var tnbObjective = Objective.None;
+                                                if (objective.UseCount != null && Others.ToInt32(objective.UseCount) > 0)
+                                                {
+                                                    tnbObjective = Objective.UseItem;
+                                                }
+                                                else if (objective.KillCount != null && Others.ToInt32(objective.KillCount) > 0)
+                                                {
+                                                    tnbObjective = Objective.KillMob;
+                                                }
+                                                else if (objective.Type == objectiveTypeType.CollectItem)
+                                                {
+                                                    tnbObjective = Objective.KillMob;
+                                                }
+                                                else if (objective.Type == objectiveTypeType.UseObject)
+                                                {
+                                                    tnbObjective = Objective.PickUpObject;
+                                                }
+                                                else if (objective.Type == objectiveTypeType.TurnIn)
+                                                    tnbObjective = Objective.TurnInQuest;
+
+                                                int collectCount = objective.CollectCount != null ? Others.ToInt32(objective.CollectCount) : 0;
+                                                int collectItemId = objective.ItemId != null ? Others.ToInt32(objective.ItemId) : 0;
+                                                int killCount = objective.KillCount != null ? Convert.ToInt32(objective.KillCount) : 0;
+                                                int mobId = objective.MobId != null ? Convert.ToInt32(objective.MobId) : 0;
+                                                var tnbHotspots = new List<Point>();
+                                                if (objective.Items != null && objective.Items.Length == 1 && objective.ItemsElementName[0] == ItemsChoiceType3.Hotspots)
+                                                {
+                                                    var hotspotses = objective.Items[0] as point3dType[];
+                                                    if (hotspotses != null)
+                                                    {
+                                                        var tnbHotspot = new Point();
+                                                        foreach (point3dType hotspot in hotspotses)
+                                                        {
+                                                            tnbHotspot.X = hotspot.X;
+                                                            tnbHotspot.Y = hotspot.Y;
+                                                            tnbHotspot.Z = hotspot.Z;
+                                                            tnbHotspots.Add(tnbHotspot);
+                                                        }
+                                                    }
+                                                }
+                                                var tmpObjective = new QuestObjective
+                                                {
+                                                    CollectCount = collectCount,
+                                                    CollectItemId = collectItemId,
+                                                    Count = killCount,
+                                                    Entry = new List<int> {mobId,},
+                                                    Name = objective.Name,
+                                                    Objective = tnbObjective,
+                                                    Hotspots = tnbHotspots,
+                                                };
+                                                /*Logging.Write("Name: " + objective.Name);
+                                                Logging.Write("CollectCount: " + objective.CollectCount);
+                                                Logging.Write("ItemId: " + objective.ItemId);
+                                                Logging.Write("KillCount: " + objective.KillCount);
+                                                Logging.Write("MobId: " + objective.MobId);
+                                                Logging.Write("ObjectId: " + objective.ObjectId);
+                                                Logging.Write("Type: " + objective.Type);
+                                                Logging.Write("UseCount: " + objective.UseCount);
+                                                tmpQuest.Objectives.Add(tmpObjective);
+                                                tnbProfile.Quests.Add(tmpQuest);
+                                            }
+                                        }
+                                        if (qContent is turninObjectiveType)
+                                        {
+                                            continue;
+                                            var turninobjective = qContent as turninObjectiveType;
+                                            Logging.Write("TurnInObjectiveType: " + turninobjective.Nav + ";" + turninobjective.X + ";" + turninobjective.Y + ";" + turninobjective.Z);
+                                        }*/
+                                
+                            }
+                        }
                     }
                 }
-            }*/
+            }
+            XmlSerializer.Serialize(Application.StartupPath + @"\test_TNB_Extract.xml", tnbProfile);
+            XmlSerializer.Serialize(Application.StartupPath + @"\test_HB_ReExtract.xml", hbProfile);
             // subProfile = hbProfile;
             /*HBProfile hbQuest = new HBProfile();
             hbQuest.Vendors = new List<HBProfile.Vendor>
@@ -216,7 +414,7 @@ public class Main : IProduct
     {
         try
         {
-            MessageBox.Show(nManager.Translate.Get(nManager.Translate.Id.No_setting_for_this_product) + ".");
+            MessageBox.Show(Translate.Get(Translate.Id.No_setting_for_this_product) + ".");
             Others.ProductStatusLog(Products.ProductName, 7);
         }
         catch (Exception e)
@@ -229,8 +427,6 @@ public class Main : IProduct
     {
         get { return _isStarted; }
     }
-
-    private bool _isStarted;
 
     #endregion
 }
