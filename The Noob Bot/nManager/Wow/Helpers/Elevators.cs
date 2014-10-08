8128f73e6f475784fa7a1be803169b523a09f5f8
TheNoobCompany <contact@thenoobcompany.com>
2014-10-08 08:56:58 +0200

    Implement class Elevators with a DB containing the 3 elevators to reach Orgrimmar Zeppelin area.

---
+++ The Noob Bot/nManager/Wow/Helpers/Elevators.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using nManager.Wow.Class;
+
+namespace nManager.Wow.Helpers
+{
+    [Serializable]
+    public class Elevators
+    {
+        public List<Elevator> Items = new List<Elevator>();
+    }
+
+    [Serializable]
+    public class Elevator
+    {
+        [XmlAttribute(AttributeName = "Id")]
+        public uint Id;
+        [XmlAttribute(AttributeName = "Name")]
+        public string Name;
+        [XmlAttribute(AttributeName = "Faction")]
+        public Npc.FactionType Faction;
+        [XmlAttribute(AttributeName = "Continent")]
+        public int ContinentId;
+        public Point BottomPoint = new Point();
+        public Point OutsideBottomPoint = new Point();
+        public Point TopPoint = new Point();
+        public Point OutsideTopPoint = new Point();
+    }
+}
