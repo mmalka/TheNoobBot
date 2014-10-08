e53c984fdae1424b6f4a21a7f1b7d37ff0f75234
TheNoobCompany <contact@thenoobcompany.com>
2014-10-08 09:20:47 +0200

    Rename class Elevators to Transports. (it can handle them all without more infos)
    Rename Bottom to A, Top to B.
    Add BContinentId; (if different... inter continental, if not, mostly elevators or intra-continentals transports)

---
+++ The Noob Bot/nManager/Wow/Helpers/Transports.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using nManager.Wow.Class;
+
+namespace nManager.Wow.Helpers
+{
+    [Serializable]
+    public class Transports
+    {
+        public List<Transport> Items = new List<Transport>();
+    }
+
+    [Serializable]
+    public class Transport
+    {
+        [XmlAttribute(AttributeName = "AContinentId")] public int AContinentId;
+        public Point APoint = new Point();
+        [XmlAttribute(AttributeName = "BContinentId")] public int BContinentId;
+        public Point BPoint = new Point();
+        [XmlAttribute(AttributeName = "Faction")] public Npc.FactionType Faction;
+        [XmlAttribute(AttributeName = "Id")] public uint Id;
+        [XmlAttribute(AttributeName = "Name")] public string Name;
+        public Point OutsideAPoint = new Point();
+        public Point OutsideBPoint = new Point();
+    }
+}
