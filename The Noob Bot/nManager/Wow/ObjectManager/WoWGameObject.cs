1fb3db44eccc58dabb27da3f2c06193159c7778d
Neo2003 <Neo.2003@Hotmail.fr>
2015-05-15 20:03:40 +0200

    Implement (untested) Tree loggin using Open Chest feature
    Signed-off-by: Neo2003 <Neo.2003@Hotmail.fr>

--- The Noob Bot/nManager/Wow/ObjectManager/WoWGameObject.cs
+++ The Noob Bot/nManager/Wow/ObjectManager/WoWGameObject.cs
@@ -632,6 +632,20 @@ namespace nManager.Wow.ObjectManager
                                 break;
 
                             case WoWGameObjectLockKeyType.LOCK_KEY_SKILL: // Do we have the skill ?
+                                if ((WoWGameObjectLockType)Row.Record.LockType[j] == WoWGameObjectLockType.LOCKTYPE_SAW_TREE)
+                                {
+                                    uint RankRequired = Row.Record.Skill[j];
+                                    Spell level1 = new Spell("Apprentice Logging");
+                                    Spell level2 = new Spell("Journeyman Logging");
+                                    Spell level3 = new Spell("Expert Logging");
+                                    if (RankRequired == 1)
+                                        return level3.KnownSpell || level2.KnownSpell || level1.KnownSpell;
+                                    else if (RankRequired == 2)
+                                        return level3.KnownSpell || level2.KnownSpell;
+                                    else if (RankRequired == 3)
+                                        return level3.KnownSpell;
+                                    return false;
+                                }
                                 SkillLine skill = SkillByLockType((WoWGameObjectLockType) Row.Record.LockType[j]);
                                 if (skill == SkillLine.None) // Lock Type unsupported by now
                                 {
