bb89cc2158b78ccd114589604eb97d282dac2110
TheNoobCompany <contact@thenoobcompany.com>
2015-06-05 22:21:58 +0300

    Add a version check unusable by hacker (as long as I don't publish a version with it... xD)
    Update to 4.7.0.

--- The Noob Bot/nManager/Information.cs
+++ The Noob Bot/nManager/Information.cs
@@ -7,7 +7,7 @@
         public const int MinWowBuild = 17128 + 1; // no need to update it
         public const int MaxWowBuild = 23000; // not to be changed until we are closer
         // The Min and Max check are in case the build offset have changed and the address return a value higher than 0, it's kind of a pattern check.
-        public const string Version = "4.6.5"; // current = 4.6.5 vs DevVersionRestrict
+        public const string Version = "MD5HashVersionForDev"; // current = 4.7.0 vs MD5HashVersionForDev
         public static string MainTitle = "The Noob Bot " + Version;
     }
 }
