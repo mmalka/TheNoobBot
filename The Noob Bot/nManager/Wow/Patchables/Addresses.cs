f659e5d216f35ef2959a640f94aadbe560d6e99e
TheNoobCompany <contact@thenoobcompany.com>
2015-09-16 05:56:41 +0300

    Update TheNoobBot to version 4.8.3. Support Wow 20490.

--- The Noob Bot/nManager/Wow/Patchables/Addresses.cs
+++ The Noob Bot/nManager/Wow/Patchables/Addresses.cs
@@ -1,7 +1,7 @@
 ﻿namespace nManager.Wow.Patchables
 {
     /// <summary>
-    ///   Offset and Pointer for Wow 20444
+    ///   Offset and Pointer for Wow 20490
     /// </summary>
     public static class Addresses
     {
@@ -244,14 +244,14 @@
         /// </summary>
         public enum FunctionWow
         {
-            ClntObjMgrGetActivePlayerObj = 0x3D1D,
-            FrameScript_ExecuteBuffer = 0x2738C,
-            CGUnit_C__InitializeTrackingState = 0x32B70E, // alias CGPlayer_C__ClickToMove
-            FrameScript__GetLocalizedText = 0x31D01C,
-            CGWorldFrame__Intersect = 0x569C1A,
-            Spell_C_HandleTerrainClick = 0x284921,
-            CGUnit_C__Interact = 0x996185,
-            strlen = 0x6B0A10, // ida _strlen
+            ClntObjMgrGetActivePlayerObj = 0x3D0F,
+            FrameScript_ExecuteBuffer = 0x2734F,
+            CGUnit_C__InitializeTrackingState = 0x32B6D4, // alias CGPlayer_C__ClickToMove
+            FrameScript__GetLocalizedText = 0x31CEF7,
+            CGWorldFrame__Intersect = 0x569EE8,
+            Spell_C_HandleTerrainClick = 0x284851,
+            CGUnit_C__Interact = 0x9963B0,
+            strlen = 0x6B0E50, // ida _strlen
             // saving
             IsOutdoors = 0x0, // ?
             UnitCanAttack = 0x0, // ?
