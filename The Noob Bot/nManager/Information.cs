d8b9875dc04249375c4c134abb39e19d6ae0c528
TheNoobCompany <contact@thenoobcompany.com>
2015-11-07 14:31:43 +0200

    Update TheNoobBot to 4.8.6.

--- The Noob Bot/nManager/Information.cs
+++ The Noob Bot/nManager/Information.cs
@@ -7,7 +7,7 @@
         public const int MinWowBuild = 17128 + 1; // no need to update it
         public const int MaxWowBuild = 25000; // not to be changed until we are closer
         // The Min and Max check are in case the build offset have changed and the address return a value higher than 0, it's kind of a pattern check.
-        public const string Version = "MD5HashVersionForDev"; // current = 4.8.5 vs MD5HashVersionForDev
+        public const string Version = "MD5HashVersionForDev"; // current = 4.8.6 vs MD5HashVersionForDev
         public static string MainTitle = "The Noob Bot " + Version;
     }
 }
