bc0892f0fb3704850ca3259139e62dc0e419fce9
TheNoobCompany <contact@thenoobcompany.com>
2016-07-19 16:50:05 +0300

    Fix ItemSubClass.db2 reading.

--- The Noob Bot/nManager/Wow/Helpers/WoWItemSubClass.cs
+++ The Noob Bot/nManager/Wow/Helpers/WoWItemSubClass.cs
@@ -1,5 +1,8 @@
-﻿using System.Runtime.InteropServices;
-using nManager.Wow.Patchables;
+﻿using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using nManager.Helpful;
 
 namespace nManager.Wow.Helpers
 {
@@ -7,27 +10,47 @@ namespace nManager.Wow.Helpers
     {
         private ItemSubClassDbcRecord _rItemSubClassRec0;
 
-        private static DBC<ItemSubClassDbcRecord> _rItemSubClassDBC;
+        private static DB5Reader _itemSubClassDB2;
+
+        private static void Init() // todo make DBC loading shared accross all others reads with a better loading class.
+        {
+            if (_itemSubClassDB2 == null)
+            {
+                var mDefinitions = DBFilesClient.Load(Application.StartupPath + @"\Data\DBFilesClient\dblayout.xml");
+                var definitions = mDefinitions.Tables.Where(t => t.Name == "ItemSubClass");
+
+                if (!definitions.Any())
+                {
+                    definitions = mDefinitions.Tables.Where(t => t.Name == Path.GetFileName("ItemSubClass"));
+                }
+                if (definitions.Count() == 1)
+                {
+                    var table = definitions.First();
+                    _itemSubClassDB2 = DBReaderFactory.GetReader(Application.StartupPath + @"\Data\DBFilesClient\ItemSubClass.db2", table) as DB5Reader;
+                    Logging.Write(_itemSubClassDB2.FileName + " loaded with " + _itemSubClassDB2.RecordsCount + " entries.");
+                }
+                else
+                {
+                    Logging.Write("DB2 ItemSubClass not read-able.");
+                }
+            }
+        }
 
         private WoWItemSubClass(string name, uint iClass)
         {
-            if (_rItemSubClassDBC == null)
-                _rItemSubClassDBC = new DBC<ItemSubClassDbcRecord>((int) Addresses.DBC.ItemSubClass);
-            for (int id = _rItemSubClassDBC.MinIndex; id <= _rItemSubClassDBC.MaxIndex; id++)
+            Init();
+            ItemSubClassDbcRecord tempItemSubClassDb2Record = new ItemSubClassDbcRecord();
+            bool found = false;
+            for (int i = 0; i < _itemSubClassDB2.RecordsCount - 1; i++)
             {
-                _rItemSubClassRec0 = _rItemSubClassDBC.GetRow(id);
-                if (_rItemSubClassRec0.Index == id)
+                tempItemSubClassDb2Record = DB5Reader.ByteToType<ItemSubClassDbcRecord>(_itemSubClassDB2.Rows.ElementAt(i));
+                if (tempItemSubClassDb2Record.LongName() == name && tempItemSubClassDb2Record.ClassId == iClass)
                 {
-                    if (_rItemSubClassRec0.ClassId == iClass)
-                    {
-                        if (LongName == name)
-                        {
-                            return;
-                        }
-                    }
+                    found = true;
+                    break;
                 }
             }
-            _rItemSubClassRec0 = new ItemSubClassDbcRecord();
+            _rItemSubClassRec0 = found ? tempItemSubClassDb2Record : new ItemSubClassDbcRecord();
         }
 
         // Factory function
@@ -41,25 +64,50 @@ namespace nManager.Wow.Helpers
             get { return _rItemSubClassRec0; }
         }
 
+        public string Name
+        {
+            get { return _rItemSubClassRec0.Name(); }
+        }
+
         public string LongName
         {
-            get
-            {
-                return _rItemSubClassDBC.String(_rItemSubClassDBC.GetRowOffset((int) _rItemSubClassRec0.Index) +
-                                                _rItemSubClassRec0.SubClassLongNameOffset +
-                                                (uint) Marshal.OffsetOf(typeof (ItemSubClassDbcRecord), "SubClassLongNameOffset"));
-            }
+            get { return _rItemSubClassRec0.LongName(); }
         }
 
         [StructLayout(LayoutKind.Sequential)]
         public struct ItemSubClassDbcRecord
         {
             public uint Index;
-            public uint ClassId;
-            public uint SubClassId;
-            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)] public uint[] Unk;
             public uint SubClassNameOffset;
             public uint SubClassLongNameOffset;
+            public ushort field0C;
+            public byte ClassId;
+            public byte SubClassId;
+            public byte field10;
+            public byte field11;
+            public byte field12;
+            public byte field13;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)] public byte[] field14;
+
+            public string Name()
+            {
+                string strValue;
+                if (_itemSubClassDB2.StringTable != null && _itemSubClassDB2.StringTable.TryGetValue((int) SubClassNameOffset, out strValue))
+                {
+                    return strValue;
+                }
+                return "";
+            }
+
+            public string LongName()
+            {
+                string strValue;
+                if (_itemSubClassDB2.StringTable != null && _itemSubClassDB2.StringTable.TryGetValue((int) SubClassLongNameOffset, out strValue))
+                {
+                    return strValue;
+                }
+                return "";
+            }
         }
     }
 }
