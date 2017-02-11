37040b0a76f829d06c17f3b81d62011658178166
Vesper <contact@thenoobcompany.com>
2017-02-11 16:20:05 +0200

    Update Transports order and update db files.

--- The Noob Bot/nManager/Wow/Helpers/Transports.cs
+++ The Noob Bot/nManager/Wow/Helpers/Transports.cs
@@ -15,21 +15,21 @@ namespace nManager.Wow.Helpers
     [Serializable]
     public class Transport
     {
-        [XmlAttribute(AttributeName = "AContinentId"), DefaultValue(0)] public int AContinentId;
-        public Point AInsidePoint = new Point();
-        public Point AOutsidePoint = new Point();
-        public Point APoint = new Point();
-        [DefaultValue(0)] public uint ALift = 0;
-        [XmlIgnore] public bool UseALift = false;
-        [XmlAttribute(AttributeName = "BContinentId"), DefaultValue(0)] public int BContinentId;
-        public Point BInsidePoint = new Point();
-        public Point BOutsidePoint = new Point();
-        public Point BPoint = new Point();
-        [DefaultValue(0)] public uint BLift = 0;
-        [XmlIgnore] public bool UseBLift = false;
-        [XmlAttribute(AttributeName = "Faction")] public Npc.FactionType Faction;
         [XmlAttribute(AttributeName = "Id")] public uint Id;
         [XmlAttribute(AttributeName = "Name")] public string Name;
+        [XmlAttribute(AttributeName = "Faction")] public Npc.FactionType Faction;
+        [XmlAttribute(AttributeName = "AContinentId"), DefaultValue(-1)] public int AContinentId = -1;
+        [XmlAttribute(AttributeName = "BContinentId"), DefaultValue(-1)] public int BContinentId = -1;
+        public Point AOutsidePoint = new Point();
+        [DefaultValue(0)] public uint ALift = 0;
+        [XmlIgnore] public bool UseALift = false;
+        public Point BOutsidePoint = new Point();
+        [XmlIgnore] public bool UseBLift = false;
+        [DefaultValue(0)] public uint BLift = 0;
+        public Point AInsidePoint = new Point();
+        public Point BInsidePoint = new Point();
+        public Point APoint = new Point();
+        public Point BPoint = new Point();
         [XmlIgnore] public bool ArrivalIsA = false;
 
         public bool ShouldSerializeAInsidePoint()
