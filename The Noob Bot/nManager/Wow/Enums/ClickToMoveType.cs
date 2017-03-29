f12c570b1a0c825747f0e56e539880dc32d7d740
Vesper <contact@thenoobcompany.com>
2017-03-29 20:10:58 +0300

    Update CTM types.

--- The Noob Bot/nManager/Wow/Enums/ClickToMoveType.cs
+++ The Noob Bot/nManager/Wow/Enums/ClickToMoveType.cs
@@ -2,23 +2,23 @@
 {
     public enum ClickToMoveType
     {
-        FaceTarget = 0x1,
-        Face = 0x2,
+        None = 0x0,
+        FaceTarget = 0x2,
+        Face = 0x3,
 
         /// <summary>
         /// Will throw a UI error. Have not figured out how to avoid it!
         /// </summary>
-        Stop_ThrowsException = 0x3,
-        Move = 0x4,
-        NpcInteract = 0x5,
-        Loot = 0x6,
-        ObjInteract = 0x7,
-        FaceOther = 0x8,
-        Skin = 0x9,
-        AttackPosition = 0xA,
-        AttackGuid = 0xB,
-        ConstantFace = 0xC,
-        None = 0xD,
+        Stop_ThrowsException = 0x4,
+        Move = 0x5,
+        NpcInteract = 0x6,
+        Loot = 0x7,
+        ObjInteract = 0x8,
+        FaceOther = 0x9,
+        Skin = 0xA,
+        AttackPosition = 0xB,
+        AttackGuid = 0xC,
+        ConstantFace = 0xD,
 
         Attack = 0x10,
         Idle = 0x13,
