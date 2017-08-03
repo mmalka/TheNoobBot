4c3fb8fca6ff59e16e133ef8eb400ab6ab8bcef2
Vesper <contact@thenoobcompany.com>
2017-08-04 00:25:31 +0300

    Display fake value for UnitFlags2 to see if similar.

--- The Noob Bot/nManager/Wow/Enums/UnitFlags.cs
+++ The Noob Bot/nManager/Wow/Enums/UnitFlags.cs
@@ -46,9 +46,41 @@ namespace nManager.Wow.Enums
     public enum UnitFlags2
     {
         None = 0,
-        FeignDeath = 0x1,
-        NoModel = 0x2,
-        ForceAutoRunForward = 0x40,
+        FeignDeath = 0x1, // legit
+        NoModel = 0x2, // legit
+        ForceAutoRunForward = 0x40, // legit
+
+        // only filling defaults values to display something
+        Influenced = 0x4,
+        PVPAttackable = 0x8,
+        Rename = 0x10,
+        Preparation = 0x20,
+        NoAttack = 0x80,
+        ImmuneToPlayer = 0x100,
+        ImmuneToNpc = 0x200,
+        Looting = 0x400,
+        PetInCombat = 0x800,
+        PvPFlagged = 0x1000,
+        Silenced = 0x2000,
+        CannotSwim = 0x4000,
+        OnlySwim = 0x8000,
+        NoAttack2 = 0x10000,
+        Pacified = 0x20000,
+        Stunned = 0x40000,
+        CanPerformActionMask1 = 0x60000,
+        InCombat = 0x80000,
+        OnTaxi = 0x100000,
+        MainHandDisarmed = 0x200000,
+        Confused = 0x400000,
+        Feared = 0x800000,
+        PossessedByPlayer = 0x1000000,
+        NotSelectable = 0x2000000,
+        Skinnable = 0x4000000,
+        Mount = 0x8000000,
+        PreventKneelingWhenLooting = 0x10000000,
+        PreventEmotes = 0x20000000,
+        Sheath = 0x40000000,
+        Sheathe = 0x40000000,
     }
 
     [Flags]
